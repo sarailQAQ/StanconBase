6a21a4423b6e862b04919e920c840a7d813839a4
sarailQAQ <sarail@qq.com>
2023-11-02 22:40:04 +0800

    Fix: show index for unique index

--- src/observer/sql/executor/show_index_executor.cpp
+++ src/observer/sql/executor/show_index_executor.cpp
@@ -64,7 +64,7 @@ RC ShowIndexExecutor::execute(SQLStageEvent *sql_event)
   for (int i = table_meta.sys_field_num(); i < table_meta.index_num(); i++) {
     const auto *index_meta = table_meta.index(i);
     for (int j = 0; j < index_meta->fields().size(); j++) {
-      oper->append({table_name, "1", index_meta->name(), to_string(j +1), index_meta->fields()[j]});
+      oper->append({table_name, to_string(index_meta->is_unique() ? 0 : 1), index_meta->name(), to_string(j +1), index_meta->fields()[j]});
     }
   }
 
